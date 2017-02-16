31f09f9f289fccdbd9f7de671adb145b76a3e678
icedream <icedream@icedream.pw>
2017-02-16 19:55:43 +0100

    Remove unused bzip2 process wrapper.

--- bzip2.go
+++ bzip2.go
@@ -2,29 +2,10 @@ package binarydist
 
 import (
 	"io"
-	"os/exec"
 
 	"github.com/dsnet/compress/bzip2"
 )
 
-type bzip2Writer struct {
-	c *exec.Cmd
-	w io.WriteCloser
-}
-
-func (w bzip2Writer) Write(b []byte) (int, error) {
-	return w.w.Write(b)
-}
-
-func (w bzip2Writer) Close() error {
-	if err := w.w.Close(); err != nil {
-		return err
-	}
-	return w.c.Wait()
-}
-
-// Package compress/bzip2 implements only decompression,
-// so we'll fake it by running bzip2 in another process.
 func newBzip2Writer(w io.Writer) (wc io.WriteCloser, err error) {
 	compressedWriter, err := bzip2.NewWriter(w, nil)
 	if err != nil {
