f923c10345a252be7537471989d32f16d45fd1bf
icedream <icedream@icedream.pw>
2017-02-16 21:03:35 +0100

    Simplify bzip2 instance creation method.

--- bzip2.go
+++ bzip2.go
@@ -7,11 +7,6 @@ import (
 )
 
 func newBzip2Writer(w io.Writer) (wc io.WriteCloser, err error) {
-	compressedWriter, err := bzip2.NewWriter(w, nil)
-	if err != nil {
-		return
-	}
-
-	wc = compressedWriter
+	wc, err = bzip2.NewWriter(w, nil)
 	return
 }
